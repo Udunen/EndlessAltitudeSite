58796ee7b9370fce305a515cbec63258a6a48e86
Udunen <cole.huffine@gmail.com>
2024-04-25 09:12:40 -0500

    ignore node_modules

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -20,6 +20,7 @@ import { Link } from "react-router-dom";
 // JavaScript plugin that hides or shows a component based on your scroll
 import Headroom from "headroom.js";
 // reactstrap components
+// eslint-disable-next-line no-unused-vars
 import {
   Button,
   UncontrolledCollapse,
